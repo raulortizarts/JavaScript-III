e7b957c967a5036bdc19c635066b3517c01a0eac
Raul Ortiz <raulortizars@gmail.com>
2019-07-09 19:41:00 -0600

    1

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global/Window Binding: Basically the root object 
+* 2. Implicit: The object before the do when a function is called
+* 3. New Binding: The object instance created by a constructor
+* 4. Explicit Binding: Using the call or apply methods
 *
 * write out a code example of each explanation above
 */
@@ -16,6 +16,7 @@
 // Principle 2
 
 // code example for Implicit Binding
+  
 
 // Principle 3
 
